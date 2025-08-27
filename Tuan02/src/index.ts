cccc8f6caa8dab2b85689b942d692008f39c17d8
H82M19\Student <Student>
2025-08-27 07:55:24 +0700

    "cau 2 Tuan 02"

--- Tuan02/src/index.ts
+++ Tuan02/src/index.ts
@@ -1,19 +1,41 @@
 //cau 1
-function sayHelloAsync(): Promise<string> {
+// function sayHelloAsync(): Promise<string> {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve(`Hello async!`);
+//         }, 2000);
+//     });
+// }
+
+// sayHelloAsync().then((message) => {
+//     console.log(message);
+// });
+
+// async function run(){
+//   const msg = await sayHelloAsync();
+//   console.log(msg);
+// }
+
+// run();
+
+// Câu 2: Hàm trả về Promise resolve với số 10 sau 1 giây
+function returnTen(): Promise<number> {
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve(`Hello async!`);
-        }, 2000);
+            resolve(10);
+        }, 1000);
     });
 }
 
-sayHelloAsync().then((message) => {
-    console.log(message);
+// Dùng .then()
+returnTen().then((num) => {
+    console.log("Trả về với then:", num);
 });
 
-async function run(){
-  const msg = await sayHelloAsync();
-  console.log(msg);
+// Hoặc dùng async/await
+async function runReturnTen() {
+    const num = await returnTen();
+    console.log("Trả về với async/await: ", num);
 }
 
-run();
+runReturnTen();
