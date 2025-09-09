00f57011debc48c8b66f51c787faa1b1c2c8ebe9
TranDanHuy <danhuymsk04@gmail.com>
2025-09-09 18:00:26 +0700

    commit ảnh 2B

--- Tuan04/Tuan04/app/(tabs)/_layout.tsx
+++ Tuan04/Tuan04/app/(tabs)/_layout.tsx
@@ -1,78 +1,158 @@
-import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import AntDesign from '@expo/vector-icons/AntDesign';
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 const styles = StyleSheet.create({
   view: {
-    backgroundColor: "#FBCB00",
-    flex: 1,
-    padding: 25,
+    flexDirection: "row",
   },
   text: {
-    fontSize: 35,
+    fontSize: 20,
     fontWeight: "bold",
-    padding: 35,
-    paddingTop: 70,
+    flexShrink: 1,
+    padding: 15,
+  },
+  text1: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  start: {
+    height: 70,
+    width: 70,
+    marginTop: 20,
   },
   view1: {
+    justifyContent: "center",
+    marginTop: 70,
+  },
+  view3: {
     flexDirection: "row",
-    borderWidth: 1,
-    borderColor: "white",
-    paddingVertical: 10,
-    padding: 18,
-    backgroundColor: "#C4C4C44D",
-    alignItems: "center",
-    marginTop: 20,
-    paddingRight: 20
-  },
-  textInput: {
-    fontWeight: "bold",
-    marginLeft: 10
-  },
-  press: {
-    marginTop: 40,
-    backgroundColor: "#060000",
-    padding: 10
-  },
-  text2:{
-    fontSize: 35,
-    color: "white",
-    textAlign: "center",
-    fontWeight: "bold",
-
+    paddingLeft: 35,
   },
   text3: {
-    marginTop: 40,
     fontWeight: "bold",
     fontSize: 25,
+  },
+  pre: {
+    flexDirection: "row",
+    gap: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
+    borderWidth: 1,
+    borderColor: "blue",
+    padding: 15,
+    borderRadius: 10,
+    alignSelf: "center",
+    paddingHorizontal: 50,
+  },
+  textInput: {
+    marginTop: 16,
+    width: "85%",
+    minHeight: 180,
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingVertical: 10,
+    textAlignVertical: "top",
+    fontSize: 20,
+    alignSelf: "center",
+    paddingHorizontal: 30,
+  },
+  watermark: {
+    bottom: 22,
+    right: 45,
+    textAlign: "right",
+    fontWeight: "bold",
+    fontSize: 14,
+  },
+  press: {
+    marginTop: 20,
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 10,
+    alignSelf: "center",
+    paddingHorizontal: 155
+  },
+  text4: {
+    fontWeight: "bold",
+    fontSize: 23,
+    color: "white",
     textAlign: "center"
-
   }
-  
 });
 
-const Layout2A = () => {
+const Layout2B = () => {
   return (
-    <View style={styles.view}>
-      <Text style={styles.text}>LOGIN</Text>
-      <View style={styles.view1}>
-        <FontAwesome name="user" size={24} color="black" />
-        <TextInput placeholder="Name" style={styles.textInput} />
+    <View>
+      <View style={styles.view}>
+        <Image source={require("../../assets/images/usb.png")} />
+        <Text style={styles.text}>
+          USB Bluetooth Music Receiver Hjx-001 - Biến loa thường thành loa
+          bluetooth
+        </Text>
       </View>
       <View style={styles.view1}>
-        <FontAwesome6 name="unlock-keyhole" size={24} color="black" />
-        <TextInput placeholder="Password" style={styles.textInput} keyboardType="numeric" maxLength={8} />
-        <AntDesign name="eye" size={24} color="black" />
+        <Text style={styles.text1}>Cực kỳ hài lòng</Text>
+        <View style={styles.view3}>
+          <Image
+            source={require("../../assets/images/OneStart.png")}
+            style={styles.start}
+          />
+          <Image
+            source={require("../../assets/images/OneStart.png")}
+            style={styles.start}
+          />
+          <Image
+            source={require("../../assets/images/OneStart.png")}
+            style={styles.start}
+          />
+          <Image
+            source={require("../../assets/images/OneStart.png")}
+            style={styles.start}
+          />
+          <Image
+            source={require("../../assets/images/OneStart.png")}
+            style={styles.start}
+          />
+        </View>
       </View>
-      <Pressable   style={({ pressed }) => [
+      <View>
+        <Pressable style={styles.pre}>
+          <FontAwesome name="camera" size={50} color="black" />
+          <Text style={styles.text3}>Thêm hình ảnh</Text>
+        </Pressable>
+        <View>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Hãy chia sẻ những điều mà bạn thích về sản phẩm"
+            multiline
+            placeholderTextColor="#9AA0A6"
+            numberOfLines={6}
+          />
+          <Text pointerEvents="none" style={styles.watermark}>
+            https://meet.google.com/nsj-ojwi-xpp
+          </Text>
+        </View>
+      </View>
+      <View>
+        <Pressable style={({ pressed }) => [
           styles.press,
           { opacity: pressed ? 0.6 : 1 },
         ]} >
-        <Text style={styles.text2}>LOGIN</Text>
-      </Pressable>
-      <Text style= {styles.text3}>Forgot your password?</Text>
+            <Text style={styles.text4}>Gửi</Text>
+
+        </Pressable>
+      </View>
+
+
     </View>
   );
 };
-export default Layout2A;
+export default Layout2B;
